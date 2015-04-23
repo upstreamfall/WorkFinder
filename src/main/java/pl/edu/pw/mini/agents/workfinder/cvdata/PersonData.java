baf8912629ac01735faabe443337d61c7f0fd783
Paweł Bielicki <bielicki91@gmail.com>
2015-04-23 20:31:27 +0200

    delete unnecessary fields

--- src/main/java/pl/edu/pw/mini/agents/workfinder/cvdata/PersonData.java
+++ src/main/java/pl/edu/pw/mini/agents/workfinder/cvdata/PersonData.java
@@ -10,18 +10,12 @@ import java.util.Properties;
 public class PersonData {
     private String name;    //John Smith
     private String email;   //john.smith@gmail.com
-    private String birthday;  //2015-04-16
-    private String phoneNumber;      //555-223-445
-    private String placeOfLiving;   //Warsaw, ul. Jana Pawła 23
 
     //TODO
     //Mozna to zrobic generycznie refleksja?
     public PersonData(Properties prop) {
         name = prop.getProperty("name");
         email = prop.getProperty("email");
-        birthday = prop.getProperty("birthday");
-        phoneNumber = prop.getProperty("phoneNumber");
-        placeOfLiving = prop.getProperty("placeOfLiving");
     }
 
     public String getName() {
@@ -32,35 +26,11 @@ public class PersonData {
         this.name = name;
     }
 
-    public String getPlaceOfLiving() {
-        return placeOfLiving;
-    }
-
-    public void setPlaceOfLiving(String placeOfLiving) {
-        this.placeOfLiving = placeOfLiving;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(String birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 }
