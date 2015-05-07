bb546696aff30b1dd71d617cb0641c0190a5bf71
Jakub Dutkowski <kdutkowski@gmail.com>
2015-05-07 21:05:28 +0200

    added yo auto generated angular application

---
+++ webApp/app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webAppApp
+ * @description
+ * # webAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webAppApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
