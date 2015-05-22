e70122f6f8a8b2242dea6ca3da6123bd68b8ee72
Jakub Dutkowski <kdutkowski@gmail.com>
2015-05-22 22:16:01 +0200

    added skills service

--- webApp/app/scripts/controllers/mydata.js
+++ webApp/app/scripts/controllers/mydata.js
@@ -1,9 +1,21 @@
 'use strict';
 
 angular.module('WorkFinderApp')
-  .controller('MydataCtrl', function ($scope, userDataService, toastr) {
+  .controller('MydataCtrl', function ($scope, userDataService, skillsDataService, toastr) {
     userDataService.getUserData().success(function (userData) {
       $scope.userData = userData;
+    });
+
+    skillsDataService.getLevels().success(function (levels) {
+      $scope.levels = levels;
+    });
+
+    skillsDataService.getSkills().success(function (skills) {
+      $scope.skills = skills;
+    });
+
+    skillsDataService.getSkillsData().success(function (skillSet) {
+      $scope.skillSet = skillSet;
     });
 
     $scope.submitUserData = function (userData) {
@@ -14,24 +26,11 @@ angular.module('WorkFinderApp')
       });
     };
 
-    $scope.levels = [
-      {id: 1, name: 'Low'},
-      {id: 2, name: 'Intermediate'},
-      {id: 3, name: 'High'},
-      {id: 4, name: 'Guru'}
-    ];
-
-    $scope.skills = [
-      {id: 1, name: 'Java'},
-      {id: 2, name: 'JS'},
-      {id: 3, name: 'MeteorJS'}
-    ];
-
-    $scope.skillSet = [
-      {skillId: 1, levelId: 2}
-    ];
-
-    $scope.print = function() {
-      console.log('dasd', $scope.skillSet);
+    $scope.submitSkillsData = function (skillsData) {
+      skillsDataService.updateSkillsData(skillsData).success(function () {
+        toastr.success('Your data has been saved!', 'Success!');
+      }).error(function () {
+        toastr.success('There was a problem while saving data', 'Error!');
+      });
     };
   });
--- webApp/app/scripts/mocks/mocks.js
+++ webApp/app/scripts/mocks/mocks.js
@@ -24,6 +24,34 @@ angular.module('WorkFinderApp')
       return [200, data];
     });
 
+    $httpBackend.whenGET('/skills').respond(function () {
+      return [200, [
+        {id: 1, name: 'Java'},
+        {id: 2, name: 'JS'},
+        {id: 3, name: 'MeteorJS'}
+      ]];
+    });
+
+    $httpBackend.whenGET('/skillset').respond(function () {
+      return [200, [
+        {skillId: 1, levelId: 2},
+        {skillId: 3, levelId: 3}
+      ]];
+    });
+
+    $httpBackend.whenGET('/levels').respond(function () {
+      return [200, [
+        {id: 1, name: 'Low'},
+        {id: 2, name: 'Intermediate'},
+        {id: 3, name: 'High'},
+        {id: 4, name: 'Guru'}
+      ]];
+    });
+
+    $httpBackend.whenPOST('/skillset').respond(function (method, url, data) {
+      return [200, data];
+    });
+
     // Catch-all pass through for all other requests
     $httpBackend.whenGET(/.*/).passThrough();
     $httpBackend.whenPOST(/.*/).passThrough();
