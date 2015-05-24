25a38e0e3746909571950376ee3d171a2daef4cd
Tomasz Janiszewski <janiszt@gmail.com>
2015-05-24 20:13:57 +0200

    Send some data back to http facade

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 
 * Build `./gradlew build`
 * Run `./gradlew execute`
-* Run WebUI `cd webUi && ./gradlew run`
+* Run WebUI `cd webUi && ../gradlew run`
 * Send message to Receiver `curl -X POST http://localhost:8080/agent/Receiver`
--- src/main/java/pl/edu/pw/mini/agents/workfinder/Receiver.java
+++ src/main/java/pl/edu/pw/mini/agents/workfinder/Receiver.java
@@ -3,14 +3,33 @@ package pl.edu.pw.mini.agents.workfinder;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
 
 public class Receiver extends ExtendedAgent {
+
+    private final static String REMOTE_URL = "http://localhost:8080/agent/%s/response";
+    private final HttpClient client = HttpClientBuilder.create().build();
 
     protected void setup() {
         //Write the greeting
         System.out.println("Hello! I'm " + getAgentName());
-        //Add the simple behaviour
         addBehaviour(new ReceiverBehaviour(this));
+    }
+
+    private void sentInfoToWebUi(String data) {
+        HttpPost post = new HttpPost(String.format(REMOTE_URL, getAgentName()));
+        try {
+            post.setEntity(new StringEntity(data));
+            client.execute(post);
+        } catch (IOException e) {
+            //This should never happened
+            throw new RuntimeException(e);
+        }
     }
 
     class ReceiverBehaviour extends CyclicBehaviour {
@@ -22,6 +41,7 @@ public class Receiver extends ExtendedAgent {
             ACLMessage msg = receive();
             if (msg != null) {
                 System.out.println("GET: " + msg);
+                sentInfoToWebUi(msg.toString());
             }
         }
     }
